e825e632e7cf5d5241d30b86085106f98e94b0b0
BotezatuAndreeaa <botezatuandreea18@stud.ase.ro>
2022-02-23 18:59:40 +0200

    Added getter, setter and constructor to Animal class

--- seminar1/src/cts/seminar1/Animal.java
+++ seminar1/src/cts/seminar1/Animal.java
@@ -2,6 +2,24 @@ package cts.seminar1;
 
 public abstract class Animal {
 	private String nume;
+	
+
+	public String getNume() {
+		return nume;
+	}
+
+
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+
+	
+
+	public Animal(String nume) {
+		super();
+		this.nume = nume;
+	}
+
 
 	abstract void mananca(String hrana);
 }
